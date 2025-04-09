73b71a9f5cdf72e71f67bf1718e17bd2948d611a
carlos.esilva107 <carlos.esilva107@JUN0675610W11-1>
2025-04-08 21:57:15 -0300

    Add project files.

---
+++ PrimeiroProjeto/Models/Cliente.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeiroProjeto.Models
+{
+    public class Cliente
+    {
+        //GET - inserir ou entregar o valor do Id
+        //SET -
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public DateTime DataCadastro { get; set; }
+        
+    }
+}
